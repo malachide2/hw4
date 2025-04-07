da25f66c582ac8f329090303b30d6542fe2e9bbe
Malachi Dewitt <mjdewitt@usc.edu>
2025-04-07 13:53:40 -0700

    Equal Paths

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -4,15 +4,27 @@
 #endif
 
 #include "equal-paths.h"
+#include <algorithm>
 using namespace std;
 
 
 // You may add any prototypes of helper functions here
-
-
-bool equalPaths(Node * root)
-{
-    // Add your code below
-
+int dfs(Node* node) {
+  if (node == nullptr)
+    return 0;
+    
+  int leftDepth = 0;
+  int rightDepth = 0;
+  if (node->left != nullptr) {
+    leftDepth = dfs(node->left);
+  }
+  if (node->right != nullptr) {
+    rightDepth = dfs(node->right);
+  }
+  return std::max(leftDepth, rightDepth) + 1;
 }
 
+bool equalPaths(Node * root) {
+  // Add your code below
+  return (abs(dfs(root->left) - dfs(root->right)) <= 1);
+}
